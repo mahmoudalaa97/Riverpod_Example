e69bee7131ffdd49279f2de6b887e7ec8800770d
mahmoud Alaa <mahmoudadmob@gmail.com>
2021-04-07 13:36:48 +0200

    [ Comment ] in Main Page

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'home_page.dart';
 import 'utils/extentions.dart';
 
+/// Main Method
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   // Firebase initialization.
@@ -12,22 +13,23 @@ void main() {
   runApp(MainApp());
 }
 
+/// MainApp Include Everything need to run like:
+/// [ ProviderScope] A widget that stores the state of providers.
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
         child: MaterialApp(
-          title: 'Flutter Demo',
-          themeMode: ThemeMode.dark,
-          darkTheme:ThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Theme.of(context).colorScheme.appBarColor,
-            appBarTheme: AppBarTheme(
-                backgroundColor: Theme.of(context).colorScheme.appBarColor
-            ),
-            scaffoldBackgroundColor: Theme.of(context).colorScheme.backgroundColor
-        ) ,
-          home: MyHomePage(),
-        ));
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Theme.of(context).colorScheme.appBarColor,
+          appBarTheme: AppBarTheme(
+              backgroundColor: Theme.of(context).colorScheme.appBarColor),
+          scaffoldBackgroundColor:
+              Theme.of(context).colorScheme.backgroundColor),
+      home: MyHomePage(),
+    ));
   }
 }
